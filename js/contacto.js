acffc271d488e5e9fb20afb7a084d193428b6313
Marco Trujillo <matrujillov@gmail.com>
2020-01-14 20:59:37 -0300

    Se agrega página de contacto
    Se agrega página de contacto con formulario respectivo

---
+++ js/contacto.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    
+    $.validate({
+        form : '#contact_form'
+    });
+
+   
+    /*cambia imagen de contacto cuando hay un error*/    
+    $('.form-grp').bind('DOMSubtreeModified', function(e) {
+
+        var hadError = $('.form-grp').hasClass('has-error');
+
+        if(hadError){
+            $('.retrato.ok').addClass('hideme').fadeOut(2600);
+            $('.retrato.errors').removeClass('hideme').fadeIn( "slow" );
+        }else{
+            $('.retrato.errors').addClass('hideme').fadeOut(2600);
+            $('.retrato.ok').removeClass('hideme').fadeIn( "slow" );
+        }
+        
+    });
+
+    $( "#contact_form" ).submit(function( event ) {
+        if($(this).valid()) {
+            $('.mensaje').removeClass('hideme');
+        }
+      });
+
+    /*submit event*/
+    $('#contact_form').submit(function(e){
+        e.preventDefautl();
+        $.ajax({
+            url: "https://formspree.io/matrujillov@gmail.com",
+            method: "POST",
+            data: $(form).serialize(),
+            dataType: "json"
+        }).done(function(){
+            alert('Tu mensaje ha sido enviado exitosamente');
+            console.log(ajax)
+        });
+        return false;
+    
+    })    
+    
+});
